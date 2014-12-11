71e9c33d2442750d78400d31683db2516c102c81
Maddin92 <martin.kroh@haw-hamburg.de>
2014-12-11 22:45:33 +0100

    Update UnitTester.h

--- Programm/Tests/UnitTester.h
+++ Programm/Tests/UnitTester.h
@@ -1,15 +1,26 @@
 /**
- * File:   UnitTester.h
+ * File: UnitTester.h
  *
  * unit-test for plugin HeightProfileCheck
  *
- * \author  Jannik Beyerstedt
+ * \author Martin Kroh
  * \version 0.1
- * \date    2014-11-25
+ * \date 2014-12-11
  */
-
 #ifndef UNIT_TESTER_H
 #define UNIT_TESTER_H
+#include <iostream>
+#include <fstream>
+#include "HeightProfileCheck.h"
+#include "StubHeightSensor.h"
 
-
+class UnitTester {
+private:
+    //HeightProfileCheck* unitUnderTest;
+    FestoProcessSensors* unitUnderTest;
+    StubHeightSensor* sensorStub;
+public:
+    UnitTester();
+    void runTest();
+};
 #endif
